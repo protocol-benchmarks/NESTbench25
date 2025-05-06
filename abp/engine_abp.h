15a24fcf72fb352a43a0eb6db41c375ff80994a8
swhitelam <50882748+swhitelam@users.noreply.github.com>
2025-05-06 10:29:20 -0700

    Add files via upload

---
+++ abp/engine_abp.h
@@ -0,0 +1,23 @@
+#ifndef ENGINE_ABP_H
+#define ENGINE_ABP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//functions exposed from engine_abp.c.
+
+void final_answer(void); //100 instances of the order parameter calculated using 10^4 trajectories
+void load_protocol(void);//reads input_control_parameters.dat
+void output_protocol(void);
+void visualize_protocol(void); //visualize the effect of the current protocol
+void load_default_protocol(void); //loads default protocol
+double calculate_order_parameter(int n_traj); //computes order parameter over n_traj trajectories
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // ENGINE_ABP_H
